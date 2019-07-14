7ad6e0f36883065fdead79b5f583af617b285537
Olawale <adedekojoseph@gmail.com>
2019-07-14 19:23:08 +0100

    chore(express): setup an exxpress app [Finishes #167279282]

---
+++ server/index.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import debug from 'debug';
+
+// initialize app
+const app = express();
+
+//Setup app to parse json and form data
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+const port = 3000;
+
+//listen on port
+app.listen(port,() => {
+
+})
