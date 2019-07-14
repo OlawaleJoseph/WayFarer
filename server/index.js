35b9dafe5fb95a84fed0a67a5dd81367c70efa74
Olawale <adedekojoseph@gmail.com>
2019-07-14 21:03:14 +0100

    chore(babel-eslint):integrate babel and eslint [Finishes #167279343]

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ import debug from 'debug';
 // initialize app
 const app = express();
 
-//Setup app to parse json and form data
+// Setup app to parse json and form data
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 const port = 3000;
 
-//listen on port
-app.listen(port,() => {
-
-})
+// listen on port
+app.listen(port, () => {
+  debug('http')(`App is listening on PORT ${port}`);
+});
