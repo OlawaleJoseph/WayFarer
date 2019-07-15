24c72ede4663f96918add5fba70b0b009497c115
Olawale <adedekojoseph@gmail.com>
2019-07-15 03:48:35 +0100

    chore(get-user-test): create test for getting a user endpoint [Delivers #167280007]

--- server/test/users.test.js
+++ server/test/users.test.js
@@ -186,4 +186,42 @@ describe('Users', () => {
       );
     });
   });
+  describe('Get A User', () => {
+    it('Should get User with given id', async () => {
+      const res = await chai
+        .request(app)
+        .get(`/api/v1/auth/users/${user.user_id}`)
+        .set('token', user.token);
+      assert.equal(res.status, 200, 'Status code should 200 for success');
+      assert.equal(res.body.data.user_id, user.user_id, 'The user id given should be equal to the user id in the response object');
+      assert.isObject(res.body.data, 'Data should be an object');
+    });
+
+    it('Should return error for invalid user id', async () => {
+      const res = await chai
+        .request(app)
+        .get('/api/v1/auth/users/c');
+      assert.equal(res.status, 400, 'Status code should 400 for error');
+      assert.equal(res.body.status, 'error');
+      assert.isString(res.body.message, 'Message should be a string');
+    });
+
+    it("A non admin user can not access another user's infomation", async () => {
+      const user4 = {
+        first_name: 'Jane',
+        last_name: 'Doe',
+        email: 'janedoe@gmail.com',
+        password: 'janedoe',
+        isAdmin: false,
+      };
+      const createdUser = await User.createUser(user4);
+      const res = await chai
+        .request(app)
+        .get(`/api/v1/auth/users/${createdUser.user_id}`)
+        .set('token', user.token);
+      assert.equal(res.status, 401, 'Status code should 400 for error');
+      assert.equal(res.body.status, 'error');
+      assert.isString(res.body.message, 'Message should be a string');
+    });
+  });
 });
