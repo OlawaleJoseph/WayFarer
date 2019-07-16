f8c19c7d82b80962eb2829ccaa72532c4bc0b0b3
Olawale <adedekojoseph@gmail.com>
2019-07-16 03:05:50 +0100

    feature(get-a-booking): create endpoint for get a booking route [Delivers #167280405]

--- server/controllers/bookings.js
+++ server/controllers/bookings.js
@@ -1,5 +1,6 @@
 import Helper from '../utils/bookings.utils';
 import Trip from '../utils/trips.utils';
+import { verifyUser } from '../utils/common';
 
 class Bookings {
   static async createBooking(req, res) {
@@ -26,6 +27,29 @@ class Bookings {
       });
     } catch (error) {
       return res.status(422).json({
+        status: 'error',
+        message: error.message,
+      });
+    }
+  }
+
+  static async findABooking(req, res) {
+    try {
+      const foundBooking = await Helper.findABooking(req.params.bookingId);
+      try {
+        verifyUser(req.user, foundBooking.user_id);
+      } catch (error) {
+        return res.status(403).json({
+          status: 'error',
+          message: error.message,
+        });
+      }
+      return res.status(200).json({
+        status: 'success',
+        data: foundBooking,
+      });
+    } catch (error) {
+      return res.status(404).json({
         status: 'error',
         message: error.message,
       });
